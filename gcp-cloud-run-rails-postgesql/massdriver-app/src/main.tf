345289b022a8a5fa6d9eae292e1a270393eb832f
Will <will@containerlabs.io>
2022-08-19 08:57:47 -0700

    rails k8s

---
+++ gcp-cloud-run-rails-postgesql/massdriver-app/src/main.tf
@@ -0,0 +1,17 @@
+module "k8s_application" {
+  source = "./modules/k8s-application"
+}
+
+resource "helm_release" "application" {
+  name             = module.k8s_application.params.md_metadata.name_prefix
+  chart            = module.k8s_application.helm_chart
+  namespace        = module.k8s_application.params.namespace
+  create_namespace = true
+  force_update     = true
+
+  values = [
+    fileexists("${path.module}/../chart/values.yaml") ? file("${path.module}/../chart/values.yaml") : "",
+    yamlencode(module.k8s_application.params),
+    yamlencode(local.helm_additional_values)
+  ]
+}
---
+++ gcp-cloud-run-rails-postgesql/massdriver-app/src/modules/k8s-application/locals.tf
@@ -0,0 +1,34 @@
+locals {
+  # directory structure in provisoner env
+  # bundle/
+  #   app.yaml
+  #   chart/
+  #   k8s-application/
+  #     shared terraform
+  #   src/
+  #     path.root is from this context
+  #     connections.auto.tfvars.json
+  #     params.auto.tfvars.json
+  #     main.tf
+  app_specification = yamldecode(file("${path.root}/../app.yaml"))
+  connections       = jsondecode(file("${path.root}/connections.auto.tfvars.json"))
+  params            = jsondecode(file("${path.root}/params.auto.tfvars.json"))
+  # this is because the CLI creates a chart directory and copies from where the app.yaml specified
+  # to this expected directory name
+  # https://github.com/massdriver-cloud/massdriver-cli/blob/1564f1bb81aadcc5221095bf9bde062e375291a0/pkg/application/package.go#L63
+  helm_chart = "${path.root}/chart"
+
+  helm_additional_values = {
+    envs = concat(
+      local.params.envs,
+      local.result_envs
+    )
+    ingress = {
+      className = "nginx" // eventually this should come from the kubernetes artifact
+      annotations = {
+        "cert-manager.io/cluster-issuer" : "letsencrypt-prod"     // eventually this should come from kubernetes artifact
+        "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true" // hardcoding this for now, dependent on nginx
+      }
+    }
+  }
+}
