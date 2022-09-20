f629535f0f5c98ff79b69a39364ca13a66a5088c
Will <will@containerlabs.io>
2022-09-20 14:11:31 -0700

    cleanup

--- k8s-express-mongo/massdriver-app/src/main.tf
+++
@@ -1,8 +0,0 @@
-module "helm_application" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm"
-  name               = var.md_metadata.name_prefix
-  namespace          = "default"
-  chart              = "${path.module}/chart"
-  kubernetes_cluster = var.kubernetes_cluster
-}
-
