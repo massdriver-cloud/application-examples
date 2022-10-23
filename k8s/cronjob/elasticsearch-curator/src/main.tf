51a013eeba21931df1c94552f74670b500166c9d
chrisghill <chris@massdriver.cloud>
2022-10-23 13:19:59 -0600

    updates

--- k8s/cronjob/elasticsearch-curator/src/main.tf
+++ k8s/cronjob/elasticsearch-curator/src/main.tf
@@ -1,9 +1,12 @@
 
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=8892201"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=9839474"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
   kubernetes_cluster = var.kubernetes_cluster
   additional_envs    = var.additionalEnvs
+  helm_additional_values = {
+    elasticsearch = var.elasticsearch
+  }
 }
