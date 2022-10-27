39c3e66b45e4564e2f0a80fac1ea5f6aef3de665
Will Beebe <will@containerlabs.io>
2022-10-27 14:11:10 -0700

    Update main.tf (#28)

--- azure/app-service/phoenix-chat-example/src/main.tf
+++ azure/app-service/phoenix-chat-example/src/main.tf
@@ -1,12 +1,12 @@
 module "application_app_service" {
-  # source        = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=9cd01fe"
-  source             = "../../../../../terraform-modules/massdriver-application-azure-app-service"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=22d422e"
   name               = var.md_metadata.name_prefix
   application        = var.application
   image              = var.image
   dns                = var.dns
   contact_email      = var.md_metadata.target.contact_email
   tags               = var.md_metadata.default_tags
+  location           = var.virtual_network.specs.azure.region
   virtual_network_id = var.virtual_network.data.infrastructure.id
   command            = var.command
 }
