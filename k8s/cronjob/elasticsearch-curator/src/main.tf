c0f23e709e2e17589dbc5467a2ef4d08b1ee92b6
chrisghill <chris@massdriver.cloud>
2022-10-28 16:49:54 -0600

    update module ref

--- k8s/cronjob/elasticsearch-curator/src/main.tf
+++ k8s/cronjob/elasticsearch-curator/src/main.tf
@@ -1,6 +1,6 @@
 
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=9839474"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=0b7eb05"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
