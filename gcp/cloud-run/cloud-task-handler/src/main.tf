36028613882b75e2c62fcf10724f69798ac15774
Will <will@containerlabs.io>
2022-12-05 18:53:52 -0800

    cloud-task creator and handler WIP

---
+++ gcp/cloud-run/cloud-task-handler/src/main.tf
@@ -0,0 +1,10 @@
+module "application_cloud_run" {
+  source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=3ee9d5f"
+  md_metadata     = var.md_metadata
+  location        = local.gcp_region
+  container_image    = "us-central1-docker.pkg.dev/md-demos/cloud-tasks/handler:${var.container.tag}"
+  container_port  = 4000
+  endpoint        = var.endpoint
+  max_instances   = var.max_instances
+  vpc_connector   = var.gcp_subnetwork.data.infrastructure.vpc_access_connector
+}
