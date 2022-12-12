c56acc7fffb6def3bccce521b80588d00951b3db
mclacore <michael@massdriver.cloud>
2022-12-12 11:30:59 -0700

    Trying to get img URLs to work

--- azure/function-app/tesseract-ocr/app/HttpOcrFunc/__init__.py
+++ azure/function-app/tesseract-ocr/app/HttpOcrFunc/__init__.py
@@ -3,11 +3,13 @@ import pytesseract
 from PIL import Image
 import azure.functions as func
 import os
+# import requests
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
 
-    # test code for OCR
+    # # test code for OCR
     try:
         file = req.files.get('file')
         file.save('/tmp/1.jpg')
@@ -19,3 +21,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         text = str(pytesseract.image_to_string(Image.open('/tmp/1.jpg')))
 
     return func.HttpResponse('text Extracted from Image: {}'.format(text))
+
+# def download_image(url, file_name):
+#     # Send GET request
+#     response = requests.get(url)
+
+#     # Save the image
+#     if response.status_code == 200:
+#         with open(file_name, "wb") as f:
+#             f.write(response.content)
+#     else:
+#         print(response.status_code)
