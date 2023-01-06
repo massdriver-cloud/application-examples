c3f6a3ce487ca295542d45b9237eba42d269ae23
Will <will@containerlabs.io>
2023-01-06 15:38:45 -0800

    add hello-world, remove tesseract-ocr for now

---
+++ azure/function/hello-world/src/main.tf
@@ -0,0 +1,19 @@
+module "application_app_service" {
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=718f586"
+  md_metadata        = var.md_metadata
+  application        = var.application
+  docker             = merge(var.docker, {
+    registry = "registry.hub.docker.com"
+    image = "karthequian/helloworld"
+  })
+  network            = {
+    auto = true
+  }
+  monitoring = {
+    mode = "AUTOMATED"
+  }
+  dns                = var.dns
+  virtual_network_id = var.azure_virtual_network.data.infrastructure.id
+  location           = var.azure_virtual_network.specs.azure.region
+  contact_email      = var.md_metadata.target.contact_email
+}
