6c157c3804ad213b06ae3f1b4e80fd4e7c4c3f89
Will Beebe <will@containerlabs.io>
2023-01-20 14:49:29 -0800

    tailscale for K8s and GCP VMs (#50)
    * tailscale for GKE and GCP VMs
    
    * dockerfile
    
    * one wait
    
    * use secret instead of static env-var, :sunglasses

---
+++ k8s/tailscale/app/start.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+
+set +ex
+
+trap 'kill -TERM $PID' TERM INT
+echo "Starting Tailscale daemon"
+# -state=mem: will logout and remove ephemeral node from network immediately after ending.
+tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} &
+PID=$!
+until tailscale up --authkey="${TS_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
+    sleep 0.1
+done
+tailscale status
+wait ${PID}
