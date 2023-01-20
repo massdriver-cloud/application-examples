591bc1860159f45faa2722b3cc7ce327955ce7a7
Will Beebe <will@containerlabs.io>
2023-01-20 11:45:20 -0800

    Adding Rust Warp Hello World (#52)
    * runs in Docker
    
    * add Massdriver wrapper, cleanup Dockerfiles

---
+++ k8s/warp-api/app/src/main.rs
@@ -0,0 +1,22 @@
+use warp::{Filter, http::Response};
+use tracing::info;
+use tracing_subscriber;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let port = env::var("PORT").unwrap_or("3000".to_string()).parse::<u16>().unwrap();
+    info!(port, "starting server");
+
+    // Match any request
+    let routes = warp::any().map(|| {
+        info!("serving request");
+        Response::builder()
+            .header("my-custom-header", "some-value")
+            .body("Hello, World!")
+    });
+
+    warp::serve(routes).run(([0, 0, 0, 0], port)).await;
+}
