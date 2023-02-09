7471dcbf12055a443bafd41ee17752ad56ee0d7d
Will Beebe <will@containerlabs.io>
2023-02-09 15:07:19 -0800

    stubbing cognitive-language example (#64)
    * stubbing cognitive-language example
    
    * bump sha

---
+++ azure/function-app/cognitive-language/app/index.js
@@ -0,0 +1,40 @@
+import { TextAnalyticsClient } from "@azure/ai-text-analytics";
+import { DefaultAzureCredential } from '@azure/identity';
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 80
+
+// Example method for detecting the language of text
+async function languageDetection() {
+   const endpoint = process.env.COGNITIVE_SERVICES_ENDPOINT;
+   if (!endpoint) throw Error('value for COGNITIVE_SERVICES_ENDPOINT not found');
+    const client = new TextAnalyticsClient(endpoint, new DefaultAzureCredential());
+
+    const languageInputArray = [
+        "Ce document est rédigé en Français."
+    ];
+    const languageResult = await client.detectLanguage(languageInputArray);
+
+    const results = [];
+    languageResult.forEach(document => {
+        console.log(`ID: ${document.id}`);
+        console.log(`\tPrimary Language ${document.primaryLanguage.name}`)
+        results.push(document);
+    });
+    return results;
+}
+
+app.get('/', (req, res) => {
+  languageDetection()
+    .then((response) => res.send(response))
+    .catch((ex) => res.send(ex.message));
+})
+
+app.get('/health', (req, res) => {
+  res.send('200: OK');
+})
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`)
+})
