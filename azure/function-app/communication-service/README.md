f39632789becbf56fc6712466b7f54770fda907a
mclacore <michael@massdriver.cloud>
2023-02-10 10:36:13 -0700

    Communication service - email function

---
+++ azure/function-app/communication-service/README.md
@@ -0,0 +1,5 @@
+# communication-service
+
+This example provisions a Function App that can connect to an Azure Communication Service bundle. This example is using the Azure Email Communication Services resource, which is still in preview. Provision an Email service resource in the Azure portal [here](https://portal.azure.com/#view/HubsExtension/BrowseResource/resourceType/Microsoft.Communication%2FEmailServices).
+
+Once the Email service is generally available, we will build the bundle to deploy this from within Massdriver.
---
+++ azure/function-app/communication-service/src/main.tf
@@ -0,0 +1,22 @@
+module "application" {
+  source      = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=86dc2b1"
+  name        = var.md_metadata.name_prefix
+  tags        = var.md_metadata.default_tags
+  application = var.application
+  docker      = var.docker
+  network = {
+    auto = true
+  }
+  health_check = {
+    path = "/health"
+    port = 80
+  }
+  monitoring = {
+    mode = "AUTOMATED"
+  }
+  dns                = var.dns
+  virtual_network_id = var.azure_virtual_network.data.infrastructure.id
+  location           = var.azure_virtual_network.specs.azure.region
+  contact_email      = var.md_metadata.target.contact_email
+  md_metadata        = var.md_metadata
+}
