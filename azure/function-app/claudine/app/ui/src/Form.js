7ea3d0d63abf07393f9593ed72dbbce220a22ae9
Will <will@containerlabs.io>
2023-02-10 20:02:23 -0800

    basic form

---
+++ azure/function-app/claudine/app/ui/src/Form.js
@@ -0,0 +1,27 @@
+// import fetch from 'fetch';
+
+function Form() {
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    fetch('https://cogy.mdazuresbx.com', { method: form.method, body: formData });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <label>Phrase: <input name="phrase" /></label>
+    </form>
+  )
+}
+
+export default Form;
