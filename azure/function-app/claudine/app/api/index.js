60804bda11bb2881160b6eff4e68f95d0a868db5
Will <will@containerlabs.io>
2023-02-10 20:52:19 -0800

    api to own folder

---
+++ azure/function-app/claudine/app/api/index.js
@@ -0,0 +1,49 @@
+import cors from 'cors';
+import express from 'express';
+import path from 'path';
+import {
+  analyzeSentiment,
+  languageDetection
+} from "./text/index.js";
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+
+  languageDetection(req)
+    .then((response) => res.send(response));
+    .catch((ex) => res.send(ex.message));
+});
+
+app.get('/sentiment', (req, res) => {
+  analyzeSentiment(req)
+    .then((response) => res.send(response));
+    .catch((ex) => res.send(ex.message));
+});
+
+app.get('/health', (req, res) => {
+  res.send('200: OK');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`);
+})
