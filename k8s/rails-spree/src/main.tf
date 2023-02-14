6f4994aa6d39391b8ae84ed5c8baa7ba629f2ab9
Cory O'Daniel <cory@coryodaniel.com>
2023-02-14 10:59:26 -0800

    rails ref update

--- k8s/rails-spree/src/main.tf
+++ k8s/rails-spree/src/main.tf
@@ -1,5 +1,5 @@
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=a1a0d8a563e7c0b2a4cdde7654b047a77a1effc5"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=f69600a394aa00bce1a1b2de50b5d959d758a0b9"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "ruby-on-rails"
