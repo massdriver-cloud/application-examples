d04ed7e7d5cb2069fa6971bb9812f329ef29717f
Will <will@containerlabs.io>
2023-02-25 02:55:23 -0800

    exams are in

--- checkerer.rb
+++ checkerer.rb
@@ -4,9 +4,12 @@ mass_yamls = files.select{|file_path| file_path.end_with?("massdriver.yaml")}
 for app_yaml in mass_yamls do
   directory = app_yaml.sub("/massdriver.yaml", "")
   # feels like it reads better to do this with the slashes...
+  has_readme = File.exists?("#{directory}/README.md")
   is_smooth_operator = File.exists?("#{directory}/operator.md")
 
   puts "Checks for #{directory}"
-  puts "Has operator.md <=> #{is_smooth_operator}" # I had to spaceship operator
+  puts "Has README.md   <=> #{has_readme}" # I had to spaceship operator
+  puts "Has operator.md <=> #{is_smooth_operator}"
+  puts "Score           <=> #{has_readme && is_smooth_operator ? "A" : (has_readme || is_smooth_operator) ? "B" : "C"}"
   puts
 end
