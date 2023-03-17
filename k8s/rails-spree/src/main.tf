02b06b67e971569345588bba9d5ec19aef083c7c
Cory O'Daniel <cory@coryodaniel.com>
2023-03-16 17:39:03 -0700

    update rails & phx chart versions

--- k8s/rails-spree/src/main.tf
+++ k8s/rails-spree/src/main.tf
@@ -1,10 +1,27 @@
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=f69600a394aa00bce1a1b2de50b5d959d758a0b9"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=e8bc74bed90c2bbadf1dfe2ebb8be949f2f47543"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "ruby-on-rails"
   helm_repository    = "https://massdriver-cloud.github.io/helm-charts/"
-  helm_version       = "0.2.1"
+  helm_version       = "0.2.3"
   kubernetes_cluster = var.kubernetes_cluster
   additional_envs    = []
+  helm_additional_values = {
+    # # The default command, args, and migration settings are below. Any
+    # # values in values.yaml can be overwritten by adding a parameter to your massdriver.yaml
+    # # or by hard coding the value here
+    #     command = ["bundle"]
+    #     args    = ["exec", "rails", "server"]
+    #     migration = {
+
+    #       enabled = true
+    #       command = ["bash", "-c"]
+    #       args = [<<ARGS
+    # bundle exec rails db:create;
+    # bundle exec rails db:migrate;
+    # ARGS
+    #       ]
+    #     }
+  }
 }
