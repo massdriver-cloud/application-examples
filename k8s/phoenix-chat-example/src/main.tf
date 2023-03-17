d15fbacdce87fd146ecf9338631517e46dc8bc5b
Cory O'Daniel <cory@coryodaniel.com>
2023-03-17 15:52:39 -0700

    template update

--- k8s/phoenix-chat-example/src/main.tf
+++ k8s/phoenix-chat-example/src/main.tf
@@ -1,21 +1,23 @@
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=f69600a394aa00bce1a1b2de50b5d959d758a0b9"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=e8bc74bed90c2bbadf1dfe2ebb8be949f2f47543"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "elixir-phoenix"
   helm_repository    = "https://massdriver-cloud.github.io/helm-charts/"
-  helm_version       = "0.1.3"
+  helm_version       = "0.1.4"
   kubernetes_cluster = var.kubernetes_cluster
   additional_envs    = []
   helm_additional_values = {
-    ## Chart values can be overwritten here:
-    # command = ["./entrypoint.sh"]
-    # args = ["mix", "phx.server"]
-    #
+    # # The default command, args, and migration settings are below. Any
+    # # values in values.yaml can be overwritten by adding a parameter to your massdriver.yaml
+    # # or by hard coding the value here
+    # command = ["mix"]
+    # args    = ["phx.server"]
+
     # migration = {
     #   enabled = true
-    #   command = ["./entrypoint.sh"]
-    #   args    = ["mix", "do", "ecto.create,", "ecto.migrate"]
+    #   command = ["mix"]
+    #   args    = ["do", "ecto.create,", "ecto.migrate"]
     # }
   }
 }
