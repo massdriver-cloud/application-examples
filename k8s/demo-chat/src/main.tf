86d7e8af91302a7179e0d8b023bbdd6254d5ff27
Cory O'Daniel <cory@coryodaniel.com>
2024-02-14 12:39:37 -0800

    demo chat

---
+++ k8s/demo-chat/src/main.tf
@@ -0,0 +1,24 @@
+module "helm" {
+  #source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=e8bc74bed90c2bbadf1dfe2ebb8be949f2f47543"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=40bbc7b"
+  name               = var.md_metadata.name_prefix
+  namespace          = var.namespace
+  chart              = "elixir-phoenix"
+  helm_repository    = "https://massdriver-cloud.github.io/helm-charts/"
+  helm_version       = "0.1.4"
+  kubernetes_cluster = var.kubernetes_cluster
+
+  helm_additional_values = {
+    # # The default command, args, and migration settings are below. Any
+    # # values in values.yaml can be overwritten by adding a parameter to your massdriver.yaml
+    # # or by hard coding the value here
+    # command = ["mix"]
+    # args    = ["phx.server"]
+
+    # migration = {
+    #   enabled = true
+    #   command = ["mix"]
+    #   args    = ["do", "ecto.create,", "ecto.migrate"]
+    # }
+  }
+}
